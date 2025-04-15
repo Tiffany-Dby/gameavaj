609b1a8ac9f5afd6abd230cf1f0d0f34fe495fc3
Clem <clement33.moreaupro@gmail.com>
2025-04-15 09:21:06 +0200

    enemy

--- core/src/main/java/fr/sup_de_vinci/gameavaj/Main.java
+++ core/src/main/java/fr/sup_de_vinci/gameavaj/Main.java
@@ -2,6 +2,8 @@ package fr.sup_de_vinci.gameavaj;
 
 import com.badlogic.gdx.Game;
 
+import fr.sup_de_vinci.gameavaj.screen.FirstScreen;
+
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
