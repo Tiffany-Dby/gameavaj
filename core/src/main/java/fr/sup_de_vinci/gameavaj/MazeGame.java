856573cd639e1bfdd4438fe102e25197786ce54b
noahBarreau <nonoferrari04@gmail.com>
2025-04-16 11:05:23 +0200

    image

--- core/src/main/java/fr/sup_de_vinci/gameavaj/MazeGame.java
+++ core/src/main/java/fr/sup_de_vinci/gameavaj/MazeGame.java
@@ -3,11 +3,12 @@ package fr.sup_de_vinci.gameavaj;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MazeGame extends ApplicationAdapter {
-    ShapeRenderer shapeRenderer;
+    SpriteBatch spriteBatch;
+    Texture whiteCellTexture;
 
     int[][] maze = {
         {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
@@ -23,7 +24,8 @@ public class MazeGame extends ApplicationAdapter {
 
     @Override
     public void create() {
-        shapeRenderer = new ShapeRenderer();
+        spriteBatch = new SpriteBatch();
+        whiteCellTexture = new Texture(Gdx.files.internal("path.png"));
     }
 
     @Override
@@ -31,32 +33,24 @@ public class MazeGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        spriteBatch.begin();
         for (int row = 0; row < maze.length; row++) {
             for (int col = 0; col < maze[0].length; col++) {
                 int x = col * cellSize;
                 int y = Gdx.graphics.getHeight() - (row + 1) * cellSize;
 
-                if (maze[row][col] == 2) {
-                    shapeRenderer.setColor(Color.BLACK);
-                    shapeRenderer.rect(x, y, cellSize, cellSize);
-                } else if (maze[row][col] == 1) {
-                    shapeRenderer.setColor(Color.WHITE);
-                    shapeRenderer.rect(x, y, cellSize, cellSize);
-
-                    shapeRenderer.setColor(Color.GREEN);
-                    float centerX = x + cellSize / 2f;
-                    float centerY = y + cellSize / 2f;
-                    float radius = cellSize / 12f;
-                    shapeRenderer.circle(centerX, centerY, radius);
+                if (maze[row][col] == 1) {
+                    spriteBatch.draw(whiteCellTexture, x, y, cellSize, cellSize);
                 }
+                // Vous pouvez ajouter d'autres textures pour les cases noires ou vertes ici
             }
         }
-        shapeRenderer.end();
+        spriteBatch.end();
     }
 
     @Override
     public void dispose() {
-        shapeRenderer.dispose();
+        spriteBatch.dispose();
+        whiteCellTexture.dispose();
     }
 }
